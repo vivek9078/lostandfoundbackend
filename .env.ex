9f9deef63fe1fa82835319e1ae32cc59cbb6ad4f
vivek9078 <148257094+vivek9078@users.noreply.github.com>
2025-06-18 01:29:24 +0530

    Update .env.ex

--- .env.ex
+++ .env.ex
@@ -1,7 +1,7 @@
-DB_HOST=db4free.net
-DB_USER=foundadmin1
+DB_HOST=sql12.freesqldatabase.com
+DB_USER=sql12785388
 DB_PASS=your_password_here
-DB_NAME=lostfounddb2
+DB_NAME=sql12785388
 DB_PORT=3306
 
 EMAIL_USER=your_email@gmail.com

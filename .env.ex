43c4e8cd8fa3c04967aec386cce10638ab6e2aaf
vivek9078 <148257094+vivek9078@users.noreply.github.com>
2025-06-25 09:50:28 +0530

    Update .env.ex

--- .env.ex
+++ .env.ex
@@ -1,7 +1,7 @@
 DB_HOST=sql12.freesqldatabase.com
-DB_USER=sql12785388
+DB_USER=sql12786697
 DB_PASS=your_password_here
-DB_NAME=sql12785388
+DB_NAME=sql12786697
 DB_PORT=3306
 
 EMAIL_USER=your_email@gmail.com
